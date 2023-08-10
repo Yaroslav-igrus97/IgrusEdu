e006a624ac47c9411d1acd3ef0ba13f96afaaf89
Яковлев Ярослав Игоревич <YIYakovlev@sberbank.ru>
2023-08-10 13:41:41 +0300

    master

---
+++ src/test/java/ru/igrus/edu/EduApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.igrus.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
